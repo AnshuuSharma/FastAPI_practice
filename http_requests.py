a16b5b72c5c0690ad900cf39e7fa3081abac2592
Anshu <anshusharma27165@gmail.com>
2026-06-23 21:12:46 +0530

    put request

--- http_requests.py
+++ http_requests.py
@@ -21,14 +21,28 @@ def create_user(username:str,password:str):
         "username":username,
         "password":password
     }
+    current_id=next_id
     next_id+=1
     
-    return {"message":f"user {username} created successfully "}
+    return {"message":f"user : {username} with id : {current_id} created successfully "}
 
 @app.get("/get_user")
-def get_user(username:str):
-    for user in fake_db.values():
-        if user["username"]==username:
-            return{"user":user["username"]}
+def get_user(user_id:str):
+    if user_id in fake_db:
+        return fake_db[user_id]
     else:
-        return{"error":"user doesnt exists"}
+        return {"error":"user doesnt exists"}
+
+
+@app.put("/modify/")
+def modify_user(user_id:str, username:str, password:str):
+
+   if user_id in fake_db.keys():
+       fake_db[user_id]["username"]=username
+       fake_db[user_id]["password"]=password
+       return{"modified successfully"}
+   else:
+       return{"error":"user not found"}
+
+
+
