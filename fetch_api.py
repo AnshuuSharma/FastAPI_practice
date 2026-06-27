9cc50f5faecac13dc9e792fa89362e8480b7b179
Anshu <anshusharma27165@gmail.com>
2026-06-28 00:27:08 +0530

    fetch api and filter result

--- fetch_api.py
+++ fetch_api.py
@@ -10,3 +10,29 @@ def fetch_api():
     response=requests.get("https://jsonplaceholder.typicode.com/posts")
 
     return response.json()
+
+@app.get("/user/{userId}")
+def get_user(userId:str):
+    response=requests.get("https://jsonplaceholder.typicode.com/posts",params={"userId":userId})
+
+    if response.status_code!=200:
+        return {"error":"couldn't fetch data"}
+
+    return response.json()
+
+
+@app.get("/even_id")
+def get_even_id():
+    response=requests.get("https://jsonplaceholder.typicode.com/posts")
+    posts=response.json()
+
+    result=[]
+
+    for post in posts:
+        try:
+            if post["id"]%2==0:
+                result.append(post["id"])
+        except KeyError:
+            continue
+
+    return result
